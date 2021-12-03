3d74e9c5a777e3eaf048789c9420c1959f0a336e
Sandi Andrian <andrian.sandi@gmail.com>
2021-12-03 20:41:55 +0700

    Initial Commit

---
+++ migrations/20211203130858-create-product.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: Sequelize.STRING,
+      description: Sequelize.TEXT,
+      stock: Sequelize.INTEGER,
+      price: Sequelize.FLOAT,
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Products');
+  }
+};
---
+++ seeders/20211203130923-initial-data-product.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var faker = require('faker')
+
+const products = [...Array(100)].map( (product) => (
+  {
+    title: faker.commerce.productName(),
+    description: faker.commerce.productDescription(),
+    stock: Math.floor(Math.random() * 100),
+    price: faker.commerce.price()
+  }
+))
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await queryInterface.bulkInsert('Products', products, {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Products', null, {})
+  }
+};
